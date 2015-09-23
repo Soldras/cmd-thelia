c64b462b11ff300f3c4945b46b0063080de7ba6c
Soldras <apenalver@openstudio.fr>
2015-09-23 14:04:09 +0200

    Update M_restart to get T_cc script after restart

--- Module/M_restart.sh
+++ Module/M_restart.sh
@@ -1,29 +1,33 @@
 #!/bin/sh
 # @author Penalver Antony
-# v0.1
+# v0.2
 
 module=$1
 
-echo  "\n==== Restart Module $module ====\n"
+echo "\n==== Restart Module $module ====\n"
 THELIA="php Thelia"
 
-echo  "\n-- Deactivation --\n"
+echo "\n-- Deactivation --\n"
 
 sudo $THELIA module:deactivate  $module
 
-echo  "\n-- Activation --\n"
+echo "\n-- Activation --\n"
 
 sudo $THELIA module:activate  $module
 
-echo  "\n==== $module is restart ====\n"
+echo "\n==== $module is restart ====\n"
 
-if [ ! hash T_cc 2>/dev/null ]; then
-	echo  "\n Can't find default Script\n"
+BASEDIR=$(dirname $0);
+CACHE=$BASEDIR"/../Global/T_cc.sh";
+echo $CACHE;
+if [ ! -x $CACHE ]; then
+	echo "\n Can't find default Script\n"
 	if [ ! -x $2 ]; then
-		echo  "\n Enter a cache script if you want\n"
+		echo "\n Enter a cache script if you want\n"
 	else
 		$2
 	fi
 else
-	echo  ${BASH_ALIASES["T_cc"]}
+	$CACHE
 fi
+
